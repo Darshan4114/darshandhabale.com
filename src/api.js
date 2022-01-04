0b2e5f1d5cbb8112e20450e77f610ce178e2d5c8
Darshan Dhabale <darshan@darshandev.tech>
2022-01-04 12:17:08 +0530

    Fixed url .mdx issue

--- src/api.js
+++ src/api.js
@@ -43,9 +43,7 @@ export function getAllPosts(fields = []) {
   const slugs = getPostSlugs()
   const posts = slugs
     .map((slug) => {
-      const post = getPostBySlug(slug, fields)
-      post.slug = slug
-      return post
+      return getPostBySlug(slug, fields)
     })
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? "-1" : "1"))
