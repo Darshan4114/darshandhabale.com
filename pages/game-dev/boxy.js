3e139e836743af4255b291ce5922ada9f422256a
Darshan Dhabale <darshan@darshandev.tech>
2022-02-21 07:31:31 +0530

    adding boxy 1

---
+++ pages/game-dev/boxy.js
@@ -0,0 +1,13 @@
+export default function Boxy() {
+  return (
+    <>
+      <p>hi</p>
+      <iframe
+        src="/boxy/index.html"
+        height={600}
+        width={900}
+        frameborder="0"
+      ></iframe>
+    </>
+  )
+}
