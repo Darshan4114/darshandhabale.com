40c3062df025ea04c8b5520eb61a0a014abdc751
Darshan Dhabale <darshan@darshandev.tech>
2022-02-24 15:48:06 +0530

    updated buildSitemap.mjs

--- src/buildTools/buildSitemap.mjs
+++ src/buildTools/buildSitemap.mjs
@@ -1,22 +1,72 @@
 import fs from "fs"
-import { join } from "path"
+import path, { join } from "path"
 import matter from "gray-matter"
-;(() => {
+
+// Recursive file list
+const readDirRecursive = async (filePath) => {
+  const dir = await fs.promises.readdir(filePath)
+  const files = await Promise.all(
+    dir.map(async (relativePath) => {
+      const absolutePath = path.join(filePath, relativePath)
+      const stat = await fs.promises.lstat(absolutePath)
+
+      return stat.isDirectory() ? readDirRecursive(absolutePath) : absolutePath
+    })
+  )
+  return files.flat()
+}
+
+;(async () => {
   const site = "https://darshandev.tech/"
+  const EXCLUDE_LIST = ["_document", "_app"]
+
+  //Processing /pages dir
+  const pagesDir = await readDirRecursive("./pages")
+  pagesDir.flat(Number.POSITIVE_INFINITY)
+  const pagesDirUrls = pagesDir
+    .map((filePath) => {
+      if (
+        filePath.slice(0, 6) === "pages/" &&
+        filePath.slice(filePath.length - 3, filePath.length) === ".js"
+      ) {
+        const url = filePath.slice(6, filePath.length - 3)
+        if (url.includes("index")) return url.replace("index", "")
+        return url
+      }
+    })
+    .filter((url) => !EXCLUDE_LIST.includes(url) && !url.includes("["))
+
+  //Processing /posts dir
   const data = fs.readdirSync("./src/mdx/posts")
   const postsDirectory = join(process.cwd(), "src/mdx/posts")
 
-  console.log(data)
-  const siteMapData = data.map((slug) => {
-    const realSlug = slug.replace(/\.mdx$/, "")
-    const fullPath = join(postsDirectory, `${realSlug}.mdx`)
-    const fileContents = fs.readFileSync(fullPath, "utf8")
-    const { data } = matter(fileContents)
-    return { url: site + realSlug, date: data.date }
-  })
+  const siteMapData = data
+    .filter((slug) => {
+      const skipList = [".DS_Store"]
+      return !skipList.includes(slug)
+    })
+    .map((slug) => {
+      const realSlug = slug.replace(/\.mdx$/, "")
+      const fullPath = join(postsDirectory, `${realSlug}.mdx`)
+      const fileContents = fs.readFileSync(fullPath, "utf8")
+      const { data } = matter(fileContents)
+      return { url: site + realSlug, date: data.date }
+    })
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pagesDirUrls
+        .map((page) => {
+          return `
+            <url>
+              <loc>${site + page}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>daily</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `
+        })
+        .join("")}
       ${siteMapData
         .map((blog) => {
           return `
@@ -29,6 +79,7 @@ import matter from "gray-matter"
           `
         })
         .join("")}
+        
       </urlset>
         `
   fs.writeFileSync("./public/sitemap.xml", sitemap)
